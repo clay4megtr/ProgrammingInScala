89e69fb5df11071eb5d18b300ec96e6a38387652
loushang <loushang@vipkid.com.cn>
2019-06-02 20:05:46 +0800

    add PartialFunction

--- src/main/scala/program_8.scala
+++ src/main/scala/program_8.scala
@@ -1,12 +1,14 @@
 import java.net.FileNameMap
 
+import org.scalatest.Matchers
+
 import scala.io.Source
 
 /**
   * 函数和闭包
   */
 
-object program_8 {
+object program_8 extends Matchers{
 
   /**
     * 函数类型：
@@ -76,7 +78,7 @@ object program_8 {
 
   (x: Int) => x + 1 //函数字面量
 
-  var increase = (x: Int) => x + 1  //把函数字面量赋值给变量increase
+  var increase = (x: Int) => x + 1 //把函数字面量赋值给变量increase
 
   increase(1) //函数的调用
 
@@ -91,47 +93,47 @@ object program_8 {
   /**
     * 函数字面量的短格式（也就是更简单的写法）
     */
-  someNumbers.filter((x) => x>0)
+  someNumbers.filter((x) => x > 0)
 
-  someNumbers.filter(x => x> 0)
+  someNumbers.filter(x => x > 0)
 
 
   /**
     * 占位符语法
     * 要求：每个参数在函数字面量仅出现一次
     */
-  someNumbers.filter(_>0)
+  someNumbers.filter(_ > 0)
 
   //如果编译器无法推断缺失的参数类型
-  val f = (_:Int) + (_:Int)
+  val f = (_: Int) + (_: Int)
 
-  f(5,10)
+  f(5, 10)
 
 
   /**
-    * 部分应用函数：偏函数
+    * 部分应用函数：
     * 可以使用单个下划线替换整个参数列表 的函数
     *
     * 部分应用函数是一种表达式，你不需要提供函数所需要的所有参数，代之以提供部分，或不提供所需参数
     *
     */
-  someNumbers.foreach(println _)  //_ 是整个参数列表的占位符，但是要在函数名和下划线之间留一个空格
+  someNumbers.foreach(println _) //_ 是整个参数列表的占位符，但是要在函数名和下划线之间留一个空格
 
 
   /**
     * scala中：当你调用函数，传入任何需要的参数，实际是在把函数应用到参数上；
     */
-  def sum(a: Int, b:Int, c:Int) = a+b+c
+  def sum(a: Int, b: Int, c: Int) = a + b + c
 
   //把函数应用到1，2，3上
-  sum(1,2,3)
+  sum(1, 2, 3)
 
   /**
     * sum的部分应用表达式， 只需要在 sum 之后放一个_即可
     */
   val a = sum _
 
-  a(1,2,3)
+  a(1, 2, 3)
 
   /**
     * 发生的事情：名为a的变量指向一个函数值对象，这个函数值是由scala编译器依照部分应用表达式sum_，自动产生的类的一个实例，
@@ -140,7 +142,7 @@ object program_8 {
     *
     * scala编译器根据表达式sum_ 自动产生的类的apply方法，简单的把这3个缺失的参数前转到sum，并返回结果
     */
-  a.apply(1,2,3)
+  a.apply(1, 2, 3)
 
   /**
     * 这种一个下划线代表全部参数列表的表达式的另一种用途，就是把它当做转换def为函数值的方式。
@@ -150,27 +152,77 @@ object program_8 {
     */
 
   //val g = processFile     这种写法是错误的，因为不能把方法或嵌套函数赋值给变量，这是不允许的
-  val g = processFile _    // 这种写法是正确的，相当于把processFile 转化成了函数值，就可以赋值给变量了
+  val g = processFile _ // 这种写法是正确的，相当于把processFile 转化成了函数值，就可以赋值给变量了
 
 
   /**
     * 还可以通过提供某些但不是全部需要的参数表达一个偏函数
     * 这个偏函数产生的类的apply()函数就只带一个参数，相当于另外两个参数变成了默认函数
     */
-  val b = sum(1,_:Int,3)
+  val b = sum(1, _: Int, 3)
 
-  println(b(2))  //实际执行1+2+3=6,b.apply(1,2,3)
+  println(b(2)) //实际执行1+2+3=6,b.apply(1,2,3)
 
-  println(b(5))  //实际执行1+5+3=9,b.apply(1,5,3)
+  println(b(5)) //实际执行1+5+3=9,b.apply(1,5,3)
 
 
   /**
     * 如果你正在写一个省略所有参数的偏程序表达，如 println _ 或 sum _ ，而且在代码的某个地方确实需要一个函数
     * 可以去掉下划线从而得到更简单的声明
     */
-  someNumbers.foreach(println _)  //所以这里传的不是函数，而是函数值；
+  someNumbers.foreach(println _) //所以这里传的不是函数，而是函数值；
 
   someNumbers.foreach(println)
+
+
+  /**
+    * 偏函数
+    * 在Scala中，模式匹配(Pattern Matching)可以用于创建偏函数(Partial Function)，偏函数是一种特殊的一元函数，
+    * 它并不会接受符合参数类型的所有可能值，而是只接受特定的值。例如，一个计算平方根的函数只接受正数而不接受负数。
+    *
+    */
+
+  //squareRoot 只接受正数，并且返回其平方根。如果传入的是负数，则会抛出运行时异常scala.MatchError。
+  val squareRoot: PartialFunction[Double, Double] = {
+    case x if x >= 0 => Math.sqrt(x)
+  }
+
+  //偏函数提供了一个 isDefinedAt 方法，可以检测一个特定的参数值是否被接受。
+  squareRoot.isDefinedAt(2) shouldEqual true
+  squareRoot.isDefinedAt(-2) shouldEqual false
+
+  //多个偏函数可以使用 orElse 或 andThen 方法串接起来，
+  val positive: PartialFunction[Int, Int] = {
+    case x if x >= 0 => x
+  }
+
+  val odd: PartialFunction[Int, Boolean] = {
+    case x if x % 2 == 1 => true
+  }
+
+  val even: PartialFunction[Int, Boolean] = {
+    case x if x % 2 == 0 => true
+  }
+
+  val evenCheck: PartialFunction[Int, Boolean] = positive andThen even
+  val oddCheck: PartialFunction[Int, Boolean] = positive andThen odd
+
+  //由于返回结果也是偏函数，所以仍然可以使用 isDefined 方法检测特定参数值是否被接受，
+  evenCheck.isDefinedAt(-2) shouldEqual false
+  evenCheck.isDefinedAt(2) shouldEqual true
+
+  //当我们想要实现一个验证系统的时候，偏函数的这个特性将会变得非常有用。我们可以实现一系列的检查用于检测输入数据是否满足特定的规则。
+
+  //这个实现方案很容易扩展，我们可以随意地从 finalCheck 上增加或减少检查项。
+  //val finalCheck = check1 andThen check2 andThen check3 ...
+
+  //在集合类上也可以使用偏函数，
+  val greaterThan20: PartialFunction[Any, Int] = {
+    case i: Int if i > 20 => i
+  }
+
+  //collect 方法接受一个类型为 PartialFunction 的偏函数，并且自动跳过未在该偏函数上定义的元素。
+  List(1, 45, 10, "blah", true, 25) collect greaterThan20 shouldEqual List(45, 25)
 
 
   /**
@@ -182,11 +234,11 @@ object program_8 {
     */
 
   var more = 1
-  val addMore = (x:Int) => x+more
+  val addMore = (x: Int) => x + more
 
 
   more = 9999
-  println(addMore(10))  //10009
+  println(addMore(10)) //10009
 
 
   //闭包对捕获变量做出的改变在闭包之外也同样可见
@@ -202,27 +254,27 @@ object program_8 {
     * ：使用的实例是那个在闭包被创建的时候活跃的
     */
 
-  def makeIncrease(more:Int) = (x:Int) => x+more
+  def makeIncrease(more: Int) = (x: Int) => x + more
 
   val incr1 = makeIncrease(1)
   val incr2 = makeIncrease(9999)
 
   //当把这些闭包应用到参数上时，回来的结果依赖于闭包被创建时more是如何创建的
   incr1(10) // 11
-  incr2(10)  //10009
+  incr2(10) //10009
 
 
   /**
     * 重复参数
     * 函数内部的类型是声明参数类型的数组，因此，这里实际是：Array[String]
     */
-  def echo(args:String*) =
+  def echo(args: String*) =
     for (arg <- args) println(arg)
 
   echo("one")
-  echo("one","two")
+  echo("one", "two")
 
-  val arr = Array("one","two","three")
+  val arr = Array("one", "two", "three")
 
   //错误写法
   //echo(arr)
@@ -236,19 +288,20 @@ object program_8 {
     * 在函数最后一个动作调用调用自己的函数，被称为尾递归
     * scala编译器检测到尾递归就用新值更新函数参数，然后把它替换成一个回到函数开头的跳转
     */
-  def approximate(guess: Double):Double =
-    if(isGoodEnough(guess)) guess
+  def approximate(guess: Double): Double =
+    if (isGoodEnough(guess)) guess
     else approximate(improve(guess))
 
   //辅助函数
-  def isGoodEnough(x:Double) = true
-  def improve(x:Double):Double = 0.0
+  def isGoodEnough(x: Double) = true
+
+  def improve(x: Double): Double = 0.0
 
 
   //while 循环的方式
-  def approximateLoop(initialGuess:Double):Double = {
+  def approximateLoop(initialGuess: Double): Double = {
     var guess = initialGuess
-    while(!isGoodEnough(guess))
+    while (!isGoodEnough(guess))
       guess = improve(guess)
     guess
   }
